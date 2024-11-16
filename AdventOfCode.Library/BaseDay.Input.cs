f8249b378b585779409a9ae466c0e7749e3f0ffc
Toni <toniesteso97@gmail.com>
2024-11-17 00:25:57 +0100

    Create base structure for project

---
+++ AdventOfCode.Library/BaseDay.Input.cs
@@ -0,0 +1,6 @@
+namespace AdventOfCode.Library;
+
+public abstract partial class BaseDay
+{
+    public abstract string Input { get; }
+}
---
+++ AdventOfCode.Library/BaseDay.cs
@@ -0,0 +1,8 @@
+namespace AdventOfCode.Library;
+
+public abstract partial class BaseDay
+{
+    public abstract int SolvePart1(string input);
+    
+    public abstract int SolvePart2(string input);
+}
