5ac2c3366e170f94013fa9ebb9bb6f6cde8f2134
Toni <toniesteso97@gmail.com>
2024-11-17 13:45:37 +0100

    Rename tests

--- AdventOfCode.Library/2023/Day01/Day01.Tests.cs
+++ AdventOfCode.Library/2023/Day01/Day01.Tests.cs
@@ -5,8 +5,8 @@ namespace AdventOfCode.Library._2023;
 public partial class Day01
 {
     [Fact]
-    public void Day01_Part1() => Assert.Equal(54697, SolvePart1(Input));
+    public void Part1() => Assert.Equal(54697, SolvePart1(Input));
     
     [Fact]
-    public void Day01_Part2() => Assert.Equal(54885, SolvePart2(Input));
+    public void Part2() => Assert.Equal(54885, SolvePart2(Input));
 }
